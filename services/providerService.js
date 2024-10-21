c41072a7335685af8b008775f805709f971d4879
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-21 15:13:15 -0300

    feat: add serviço e editar (com problemas no android)

--- services/providerService.js
+++ services/providerService.js
@@ -1,14 +1,9 @@
 import api from "./api"
 
 export const addservice = async (formData, token) => {
-  return await api.post("/user/provider/addservice", {
-    formData,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return await api.post("/user/provider/addservice", formData, token)
 }
 
-export const editservice = async (formData) => {
-  return await api.patch("/user/provider/editservice", { formData })
+export const editservice = async (formData, id, token) => {
+  return await api.patch(`/user/provider/editservice/${id}`, formData, token)
 }
