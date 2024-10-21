62f74ba06065dd74a3954c9a19330e084b94e8a9
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-21 15:11:07 -0300

    refactor: metodos http aceita multipart e json

--- services/api.js
+++ services/api.js
@@ -1,28 +1,69 @@
 const BASE_URL = "http://192.168.1.4:3000" // IP PODE VARIAR
 
 const api = {
-  get: async (path) => {
-    const response = await fetch(`${BASE_URL}${path}`)
+  get: async (path, token) => {
+    const headers = {
+      "Content-Type": "application/json",
+    }
+
+    // Adiciona o token ao cabeçalho apenas se ele existir
+    if (token) {
+      headers.Authorization = `Bearer ${token}`
+    }
+
+    const response = await fetch(`${BASE_URL}${path}`, {
+      method: "GET",
+      headers: headers,
+    })
+
     return response.json()
   },
-  post: async (path, data) => {
+  post: async (path, data, token) => {
+    const headers = {}
+
+    if (token) {
+      headers.Authorization = `Bearer ${token}`
+    }
+
+    let body
+
+    if (data instanceof FormData) {
+      body = data
+    } else {
+      headers["Content-Type"] = "application/json"
+      body = JSON.stringify(data)
+    }
+
     const response = await fetch(`${BASE_URL}${path}`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      headers: headers,
+      body: body,
     })
+
     return response.json()
   },
-  patch: async (path, data) => {
+  patch: async (path, data, token) => {
+    const headers = {}
+
+    if (token) {
+      headers.Authorization = `Bearer ${token}`
+    }
+
+    let body
+
+    if (data instanceof FormData) {
+      body = data
+    } else {
+      headers["Content-Type"] = "application/json"
+      body = JSON.stringify(data)
+    }
+
     const response = await fetch(`${BASE_URL}${path}`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      headers: headers,
+      body: body,
     })
+
     return response.json()
   },
   delete: async (path) => {
