bdbecb40cb4bc89d4e4771f7e8d8a487aa272940
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-30 17:14:13 -0300

    feat: adicionado metodo de edição do usuario

--- services/clientService.js
+++ services/clientService.js
@@ -3,6 +3,10 @@ import api from "./api"
 export const getUserProfile = async (token) => {
   return await api.get("/user/client/get-user-profile", token)
 }
+
+export const editUser = async (formData, id, token) => {
+  return await api.patch(`/user/client/edit/${id}`, formData, token)
+}
 export const sendActualLocation = async (address, token) => {
   return await api.post("/user/client/send-actual-location", { address }, token)
 }
