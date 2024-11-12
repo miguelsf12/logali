7dd151537b8f7a542b3a7e7d2427313b1a584f4e
Miguel Santos <miguelsantosf12@gmail.com>
2024-11-12 15:34:09 -0300

    refactor: de formData para form

--- services/authService.js
+++ services/authService.js
@@ -1,15 +1,15 @@
 import api from "./api"
 
-export const login = async (formData) => {
-  return await api.post("/user/auth/login", { formData })
+export const login = async (form) => {
+  return await api.post("/user/auth/login", form)
 }
 
-export const register = async (formData) => {
-  return await api.post("/user/auth/register", { formData })
+export const register = async (form) => {
+  return await api.post("/user/auth/register", form)
 }
 
 export const check_auth = async (token) => {
-  return await api.get("/user/auth/check-auth")
+  return await api.get("/user/auth/check-auth", token)
 }
 
 export const changePassword = async (form) => {
