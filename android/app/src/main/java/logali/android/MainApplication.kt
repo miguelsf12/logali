93846fa60d2b7ee226a3d400ff16e7f1ab2e2e24
Miguel Santos <miguelsantosf12@gmail.com>
2024-12-04 12:36:54 -0300

    android path

--- android/app/src/main/java/logali/android/MainApplication.kt
+++ android/app/src/main/java/logali/android/MainApplication.kt
@@ -29,7 +29,7 @@ class MainApplication : Application(), ReactApplication {
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-        
+
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
           override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
