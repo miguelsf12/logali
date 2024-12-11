d5aa2311457dd10a48cebe276c050c4f86c7f460
Miguel Santos <miguelsantosf12@gmail.com>
2024-12-11 15:08:18 -0300

    refactor: Posição do texto e botoes alterados

--- app/WelcomeScreen.jsx
+++ app/WelcomeScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
-import Button from "../components/Button"
 import background from "../assets/images/background.jpeg"
 import { useRouter } from "expo-router"
 
@@ -21,37 +20,52 @@ export default function WelcomeScreen() {
           end={{ x: 0.5, y: 0 }}
           style={styles.gradientOverlay}
         />
-        <Text style={styles.title}>A sua solução está Logali!</Text>
-        <View style={styles.authContainer}>
-          <Button text={"LOGIN"} href={"/LoginScreen"} />
-          <Button text={"REGISTER"} href={"/RegisterScreen"} />
-          {/* <TouchableOpacity
-            style={{
-              backgroundColor: "#607AFB",
-              borderRadius: 16,
-              paddingVertical: 12,
-              alignItems: "center",
-            }}
-            onPress={() => router.push("LoginScreen")}
-          >
-            <Text style={{ color: "#F9FAFA", fontWeight: "bold", fontSize: 16 }}>
-              Login
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#607AFB",
-              borderRadius: 16,
-              paddingVertical: 12,
-              alignItems: "center",
-            }}
-            onPress={() => router.push("RegisterScreen")}
-          >
-            <Text style={{ color: "#F9FAFA", fontWeight: "bold", fontSize: 16 }}>
-              Registrar
-            </Text>
-          </TouchableOpacity> */}
-
+        <View style={styles.content}>
+          <Text style={styles.title}>A sua solução está Logali!</Text>
+          <View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#fff",
+                padding: 20,
+                width: 250,
+                borderRadius: 50,
+                marginBottom: 20,
+              }}
+              onPress={() => router.push("LoginScreen")}
+            >
+              <Text
+                style={{
+                  color: "black",
+                  fontWeight: "bold",
+                  fontSize: 16,
+                  textAlign: "center",
+                }}
+              >
+                ENTRAR
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#fff",
+                padding: 20,
+                width: 250,
+                borderRadius: 50,
+                marginBottom: 20,
+              }}
+              onPress={() => router.push("RegisterScreen")}
+            >
+              <Text
+                style={{
+                  color: "black",
+                  fontWeight: "bold",
+                  fontSize: 16,
+                  textAlign: "center",
+                }}
+              >
+                REGISTRAR
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ImageBackground>
     </View>
@@ -61,8 +75,6 @@ export default function WelcomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "transparent",
   },
   imageBackground: {
     flex: 1,
@@ -71,19 +83,18 @@ const styles = StyleSheet.create({
   gradientOverlay: {
     ...StyleSheet.absoluteFillObject,
   },
-  title: {
-    position: "absolute",
-    left: 70,
-    color: "#fff",
+  content: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
     alignSelf: "center",
+    gap: 80,
+    marginBottom: 80,
+  },
+  title: {
+    color: "#fff",
     fontSize: 52,
     fontWeight: "800",
     width: 250,
-  },
-  authContainer: {
-    position: "absolute",
-    bottom: "8%",
-    marginTop: 10,
-    alignSelf: "center",
   },
 })
