091933eca90356d644247243b4484c36fc65c58a
Miguel Santos <miguelsantosf12@gmail.com>
2024-12-16 17:01:27 -0300

    env add

---
+++ app.config.js
@@ -0,0 +1,53 @@
+import "dotenv/config"
+
+export default {
+  expo: {
+    name: "logali",
+    slug: "logali",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "logali.ios",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY,
+        },
+      },
+      package: "logali.android",
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router"],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "911ee544-3c46-49fd-a80b-7945e70c5eb9",
+      },
+    },
+    newArchEnabled: true,
+  },
+}
+
