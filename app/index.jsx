75449ab6f94642659ee2dd333d4376558fee31e0
Miguel Santos <miguelsantosf12@gmail.com>
2024-12-19 10:37:07 -0300

    refactor: imagem logo alterada

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
-import { View, ActivityIndicator, ImageBackground, Text, Image } from "react-native"
+import { View, Image, StyleSheet } from "react-native"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { useRouter } from "expo-router"
-import logo from "../assets/images/logo-logali.png"
+import logo from "../assets/images/splash.png"
 import { check_auth } from "../services/authService"
 
 export default function Index() {
@@ -14,30 +14,29 @@ export default function Index() {
       const token = await AsyncStorage.getItem("authToken")
 
       // Aguarda 2 segundos antes de redirecionar
-      setTimeout(async () => {
-        if (token) {
-          try {
-            const response = await check_auth(token)
-            console.log(response)
 
-            if (response.userId) {
-              // Se o token for válido, redireciona para HomeScreen
-              router.replace("/(tabs)/HomeScreen")
-            } else {
-              // Se o token não for válido, redireciona para WelcomeScreen
-              await AsyncStorage.removeItem("authToken")
-              router.replace("/WelcomeScreen")
-            }
-          } catch (error) {
-            console.error("Erro ao verificar autenticação:", error)
-            router.replace("/WelcomeScreen") // Caso ocorra um erro, redireciona
+      if (token) {
+        try {
+          const response = await check_auth(token)
+          console.log(response)
+
+          if (response.userId) {
+            // Se o token for válido, redireciona para HomeScreen
+            router.replace("/(tabs)/HomeScreen")
+          } else {
+            // Se o token não for válido, redireciona para WelcomeScreen
+            await AsyncStorage.removeItem("authToken")
+            router.replace("/WelcomeScreen")
           }
-        } else {
-          router.replace("/WelcomeScreen")
+        } catch (error) {
+          console.error("Erro ao verificar autenticação:", error)
+          router.replace("/WelcomeScreen") // Caso ocorra um erro, redireciona
         }
+      } else {
+        router.replace("/WelcomeScreen")
+      }
 
-        setLoading(false) // Conclui o carregamento
-      }, 2000) // 2000 milissegundos = 2 segundos
+      setLoading(false) // Conclui o carregamento
     }
 
     checkAuth()
@@ -45,18 +44,24 @@ export default function Index() {
 
   if (loading) {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#216A8B",
-        }}
-      >
-        <Image source={logo}></Image>
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
       </View>
     )
   }
 
   return null
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#216A8B",
+  },
+  logo: {
+    width: "100%", // Largura total da tela
+    height: "100%", // Altura total da tela
+  },
+})
